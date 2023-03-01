d10cb16d73ee1e99468cef5617ae6c7cc3d5faf6
Jack Sari <jack.sanchez@alarcongroup.us>
2023-03-01 15:45:47 -0500

    start commit

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,24 @@ import axios, { AxiosError } from 'axios';
 import './interfaces/index';
 import { IConfigSmartRooster } from './interfaces/index';
 
-export interface IConfigApi {
+export interface IConfigSAle {
   perpage: number;
   page: number;
-  category_id?: number;
+}
+export interface IConfigPublic {
+  perpage: number;
+  page: number;
+  category_id: number;
+}
+export interface IConfigApiDetails {
+  specimen_id: string;
+}
+export interface IReservedSpecimen {
+  specimen_id: string;
 }
 
 export class SmartRoosterApi {
-  constructor(private readonly config: IConfigSmartRooster) {
-    config.url = config.production
-      ? 'https://back.smartroosterbureau.us'
-      : // : 'https://preback.smartroosterbureau.us/';
-        'http://192.168.1.14:9000';
-    // console.log('init', config);
-  }
+  constructor(private readonly config: IConfigSmartRooster) {}
 
   existToken() {
     if (this.config.private_key && this.config.public_key) {
@@ -36,8 +40,8 @@ export class SmartRoosterApi {
     try {
       const { data } = await axios(`${this.config.url}/service/access`, {
         headers: {
-          private_key: this.config.private_key,
-          public_key: this.config.public_key,
+          'PRIVATE-KEY': this.config.private_key,
+          'PUBLIC-KEY': this.config.public_key,
         },
       });
       return data;
@@ -47,14 +51,14 @@ export class SmartRoosterApi {
       return {
         data: [],
         message:
-          err.response?.status == 401 ? 'Token incorrecto' : 'Algo fallo',
+          err.response?.status == 401 ? 'Token incorrecto' : err.response?.data,
         success: false,
         status: false,
       };
     }
   }
 
-  async getSale(body: IConfigApi) {
+  async getSale(body: IConfigSAle) {
     if (!this.existToken()) {
       return {
         ok: false,
@@ -63,15 +67,12 @@ export class SmartRoosterApi {
     }
 
     try {
-      const { data } = await axios(
-        `${this.config.url}/service/sale`,
-        {
-          ...body,
-        },
+      const { data } = await axios.get(
+        `${this.config.url}/service/sale?page=${body.page}&perpage=${body.perpage}`,
         {
           headers: {
-            private_key: this.config.private_key,
-            public_key: this.config.public_key,
+            'PRIVATE-KEY': this.config.private_key,
+            'PUBLIC-KEY': this.config.public_key,
           },
         }
       );
@@ -82,25 +83,170 @@ export class SmartRoosterApi {
       return {
         data: [],
         message:
-          err.response?.status == 401 ? 'Token incorrecto' : 'Algo fallo',
+          err.response?.status == 401 ? 'Token incorrecto' : err.response?.data,
+        success: false,
+        status: false,
+      };
+    }
+  }
+
+  async getSaleDetails(body: IConfigApiDetails) {
+    if (!this.existToken()) {
+      return {
+        ok: false,
+        message: 'Sin token',
+      };
+    }
+
+    try {
+      const { data } = await axios.get(
+        `${this.config.url}/service/sale/details/${body.specimen_id}`,
+        {
+          headers: {
+            'PRIVATE-KEY': this.config.private_key,
+            'PUBLIC-KEY': this.config.public_key,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      const err = error as AxiosError;
+
+      return {
+        data: [],
+        message:
+          err.response?.status == 401 ? 'Token incorrecto' : err.response?.data,
+        success: false,
+        status: false,
+      };
+    }
+  }
+
+  async getPublic(body: IConfigPublic) {
+    if (!this.existToken()) {
+      return {
+        ok: false,
+        message: 'Sin token',
+      };
+    }
+
+    try {
+      const { data } = await axios.get(
+        `${this.config.url}/service/public/${body.category_id}?page=${body.page}&perpage=${body.perpage}`,
+        {
+          headers: {
+            'PRIVATE-KEY': this.config.private_key,
+            'PUBLIC-KEY': this.config.public_key,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      const err = error as AxiosError;
+
+      return {
+        data: [],
+        message:
+          err.response?.status == 401 ? 'Token incorrecto' : err.response?.data,
+        success: false,
+        status: false,
+      };
+    }
+  }
+
+  async getPublicDetails(body: IConfigApiDetails) {
+    if (!this.existToken()) {
+      return {
+        ok: false,
+        message: 'Sin token',
+      };
+    }
+
+    try {
+      const { data } = await axios.get(
+        `${this.config.url}/service/public/details/${body.specimen_id}`,
+        {
+          headers: {
+            'PRIVATE-KEY': this.config.private_key,
+            'PUBLIC-KEY': this.config.public_key,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      const err = error as AxiosError;
+
+      return {
+        data: [],
+        message:
+          err.response?.status == 401 ? 'Token incorrecto' : err.response?.data,
+        success: false,
+        status: false,
+      };
+    }
+  }
+
+  async getCategoriesSpecimens() {
+    if (!this.existToken()) {
+      return {
+        ok: false,
+        message: 'Sin token',
+      };
+    }
+
+    try {
+      const { data } = await axios.get(
+        `${this.config.url}/service/public/get-categories`,
+        {
+          headers: {
+            'PRIVATE-KEY': this.config.private_key,
+            'PUBLIC-KEY': this.config.public_key,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      const err = error as AxiosError;
+
+      return {
+        data: [],
+        message:
+          err.response?.status == 401 ? 'Token incorrecto' : err.response?.data,
+        success: false,
+        status: false,
+      };
+    }
+  }
+
+  async reservedSpecimen(body: IReservedSpecimen) {
+    if (!this.existToken()) {
+      return {
+        ok: false,
+        message: 'Sin token',
+      };
+    }
+
+    try {
+      const { data } = await axios.put(
+        `${this.config.url}/service/sale/update-reservated/${body.specimen_id}`,
+        {
+          headers: {
+            'PRIVATE-KEY': this.config.private_key,
+            'PUBLIC-KEY': this.config.public_key,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      const err = error as AxiosError;
+
+      return {
+        data: [],
+        message:
+          err.response?.status == 401 ? 'Token incorrecto' : err.response?.data,
         success: false,
         status: false,
       };
     }
   }
 }
-
-// Route::group(['middleware' => ['key-client']], function () {
-//   Route::get('/access', function () {
-//       return response()->json(["message" => "Authorized"], 200);
-//   });
-//   Route::prefix('sale')->group(function () {
-//       Route::get('/', [SpecimensServiceController::class, 'getSpecimensSale']);
-//       Route::get('/details/{specimen_id}', [SpecimensServiceController::class, 'getSpecimenSaleDetails']);
-//   });
-//   Route::prefix('public')->group(function () {
-//       Route::get('/{category_id}', [SpecimensServiceController::class, 'getSpecimensPublic']);
-//       Route::get('/get-categories', [SpecimensServiceController::class, 'getCategoriesSpecimens']);
-//       Route::get('/details/{uuid}', [SpecimensServiceController::class, 'getSpecimenPublicDetails']);
-//   });
-// });
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,5 @@
 export interface IConfigSmartRooster {
     private_key: string,
     public_key: string,
-    production: boolean,
     url?: string,
 }
